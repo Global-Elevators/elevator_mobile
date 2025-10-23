16d1583dd8f035ada0d00bf540e6a5d8c62cd549
Moamen <mmoamenmohamed101@gmail.com>
2025-10-23 10:51:58 +0300

    flavors

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'app/app.dart';
 
+// This is test branch
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
